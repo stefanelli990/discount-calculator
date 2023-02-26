eddd8ba707a5e2cb3216a28e22b2faa017741e44
stefanelli990 <stefan.mohenski@gmail.com>
2023-02-26 23:31:39 +0100

    added few js functions and styled completely

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const originalPriceInput = document.getElementById("originalPriceInput");
 const discountPrecentageInput = document.getElementById("discountPrecentageInput");
 const finalPrices = document.getElementById("finalPrices");
+const btn = document.querySelector('.btn');
+const inputs = document.querySelectorAll('input');
 
 // calculate discount
 const calculateDiscount = () => {
@@ -15,7 +17,27 @@ const calculateDiscount = () => {
   console.log({ finalPrice: finalPrice, yourSavings: savings });
 
   finalPrices.innerHTML = `
-    <h2>Final price is ${finalPrice.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2,})}</h2>
-    <p>Your savings will be ${savings.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2,})}</p>
+    <h2>Final price is <span>${finalPrice.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2,})}</span></h2>
+    <p>Your savings will be <span>${savings.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2,})}</span></p>
    `;
 };
+
+// reset inputs 
+btn.addEventListener('click', () => {
+  inputs.forEach(input => {
+    input.value = '';
+  })
+  document.querySelectorAll('.app-results span').forEach(span => {
+    console.log(span)
+    span.textContent = '0.00';
+  })
+});
+
+// prevent from typing letters
+inputs.forEach(input => {
+  input.addEventListener('input', () => {
+          if (isNaN(Number(input.value))) {
+      input.value = '';
+    }
+  })
+});
